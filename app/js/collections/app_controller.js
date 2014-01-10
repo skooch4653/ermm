0c48103d1ad710e821ffe67aad0c6a233681447d
Steve Young <syoung@domanistudios.com>
2014-01-10 11:23:11 -0500

    optional settings hash to configureServer, router decoupling

--- app/js/collections/app_controller.js
+++ app/js/collections/app_controller.js
@@ -13,39 +13,62 @@ define([
     // initialize the server
     function initialize(){
       var server = express(),
-        PORTNUMBER = 3000;
+        port = 3000;
 
-      http.createServer(server).listen(PORTNUMBER, function(){
-        console.log('Server is listening on port 3000.');
-        configureServer(server, PORTNUMBER);
+      http.createServer(server).listen(port, function(){
+        // console.log('Server is listening on port 3000.');
+        configureServer(server, port, {
+          standardIO: true
+        });
       });
     }
 
     // configure the server
-    function configureServer(server, port){
+    var configureServer = function(server, port, settings){
+      var router = new Router();
+
       //! path.dirname(module.uri) used instead of __dirname due to requirejs known compat. ticket
       server.use(express.static(path.join(path.dirname(module.uri), 'public')));
+      router.enableRoutes(server);
 
-      // routes, to be decoupled...
-      server.get('../../public/main.html', function(request, response){
-        response.send('Welcome to the app home page!');
-      });
+      if (settings) {
+        if (settings.standardIO && typeof settings.standardIO === 'boolean') {
+          enableStandardInput(settings.standardIO);
+        }
+      }
 
-      server.get('/', function(){
-        response.send('Hey!');
-      });
-
-      // explain yourself!!
-      explainConfiguration(port);
+      settings ? explainConfiguration(port, settings): explainConfiguration(port);
     }
 
     // explain how the server was configured
-    function explainConfiguration(port){
-      console.log('---------------------------------------');
+    var explainConfiguration = function(port, settings){
+      // pretties explaining how the server has been configured
+      console.log('\n---------------------------------------');
       console.log('Server has been configured and is now running at http://127.0.0.1:'
         + String(port)+'/');
+
+      // require/node global reads
+      console.log(process.execPath);
+      console.log('node ' + process.version);
+      console.log(process.platform);
+
+      if (settings) {
+        console.log('\noptional settings ~~~~~~~~~~~~~~~~~~~~~');
+        console.log('stdio streams enabled = ' + settings.standardIO);
+      } else {
+        console.log('No optional configuration settings detected.');
+      }
       console.log('---------------------------------------');
       return this; // more chaining...
+    }
+
+    var enableStandardInput = function(enabled){
+      if (enabled) {
+        process.stdin.resume();
+        process.stdin.on('data', function (chunk) {
+          process.stdout.write('data: ' + chunk);
+        });
+      }
     }
 
     return {
--- app/js/routes/router.js
+++ app/js/routes/router.js
@@ -3,12 +3,23 @@
 define([], function(){
 
   var Router = function(){
+
+    var enableRoutes = function(server) {
+      server.get('../../public/main.html', function(request, response){
+        response.send('Welcome to the app home page!');
+      });
+
+      server.get('/', function(){
+        response.send('Hey!');
+      });
+    }
+
     var routes = {
       home: '/'
     }
 
     return {
-      routes: routes
+      enableRoutes: enableRoutes
     }
   };
 
