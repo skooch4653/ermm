741c1b910dde93cf016b857c7f27b065409c95ac
Steve Young <syoung@domanistudios.com>
2014-01-19 16:01:01 -0500

    basic file server for js and css, need DRY decoup

--- app/routes/router.js
+++ app/routes/router.js
@@ -20,38 +20,37 @@ module.exports = function(){
     server.get('/', function(request, response){
        // to be filled by database
       response.render('home', {title: 'welcome to express'});
-      talkAbout(request, response); // make a promise!
     });
 
     //! handling hashes
     server.get('/*/#:id', function(request, response){
       response.send(request.params.id + ' recognized as submitted hashtag modification.');
-      talkAbout(request, response);
+      writeAbout(request, response);
     });
 
     // GET help
     server.get('/help/:issue', function(request, response){
       response.send(request.params.issue + ' detected as issue.');
-      talkAbout(request, response);
+      writeAbout(request, response);
     });
 
     // GET chapters
     server.get('/chapter/[1-9]{1,4}/:node', function(request, response){
       response.send(request.params.node + ' is the current node.');
-      talkAbout(request, response);
+      writeAbout(request, response);
     });
 
     // POST chapters
     server.post('/chapter/[1-9]{1,4}/:page', function(request, response){
       response.send('page ' + request.params.page);
-      talkAbout(request, response);
+      writeAbout(request, response);
     });
 
-    /* very rough, manual route. needs fix */
+    /* very rough, manual file serve. needs fix */
     // handling templates calling for css
     server.get('/css/*', function(request, response){
-      if(request.url.indexOf('.css') != -1){ // does the request url contain a *.css filename
-        fs.readFile(path.normalize(__dirname + '/../public/css/style.min.css'), function (err, data) {
+      if (request.url.indexOf('.css') || request.url.indexOf('.less') !== -1){ // does the request url contain a *.css filename
+        fs.readFile(path.normalize(__dirname + '/../public/' + request.url), function (err, data) {
           if (err) console.log(err);
           response.writeHead(200, {'Content-Type': 'text/css'});
           response.write(data);
@@ -60,15 +59,29 @@ module.exports = function(){
       }
     });
 
+    server.get('/js/*', function(request, response){
+      if (request.url.indexOf('.js') !== -1){ // does the request url contain a *.js filename
+        fs.readFile(path.normalize(__dirname + '/../public/' + request.url), function (err, data) {
+          if (err) console.log(err);
+          response.writeHead(200, {'Content-Type': 'text/javascript'});
+          response.write(data);
+          writeAbout(request, response);
+          response.end();
+        });
+      }
+    });
+
+
   };
 
-  var talkAbout = function(request, response){
-    // url, setTimout, headers, httpVersion, statusCode, method, socket.bytesRead
-    console.log('status: ' + request.statucCode);
-    console.log('method: ' + request.method);
-    console.log('url: ' + request.url);
-    console.log('request params: ' + request.params);
-    console.log('--\t');
+  var fetchPath = function(request, fileType){
+    // brilliant code
+  };
+
+  var writeAbout = function(request, response){
+    console.log(request.method + ' '
+      + response.statusCode + ' '
+      + request.url);
   };
 
   // public methods
