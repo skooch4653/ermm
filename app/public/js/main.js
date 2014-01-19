c15d9656d5449fa20ba5d049a53e8f3dd884bd9d
Steve Young <syoung@domanistudios.com>
2014-01-19 16:01:39 -0500

    synergistic changes with handlebars and the router

--- app/public/js/main.js
+++ app/public/js/main.js
@@ -1,28 +1,6 @@
-/* @ author: Hilly */
+/* @ author: Steve Young */
 
-require.config({
-  paths : {
-    "backbone": ["vendor/backbone/backbone-min"],
-    "jasmine": ["vendor/jasmine/lib/jasmine-core/jasmine"],
-    "jquery": ["vendor/jquery/jquery.min"],
-    "less": ["vendor/less/dist/less-1.6.0.min"],
-    "underscore": ["vendor/underscore/underscore-min"]
-  },
-  shim : {
-    'backbone' : {
-      deps: ['jquery', 'underscore'],
-      exports : 'Backbone'
-    },
-    'underscore' : {
-      exports: '_'
-    }
-  }
-});
-
-
-require(['less'], function(less) {
-  var ERMM = function() {
-    less.watch()
-    console.log('Welcome to ERMM!');
-  }();
-});
+var Chaptr = function() {
+  //less.watch()
+  console.log('Welcome to Chaptr!');
+}();
